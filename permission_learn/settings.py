87fe4386fe4e3021f232c1c0fff4ecf9479d9437
Edward <edwardlshfzw@gmail.com>
2019-02-26 17:21:57 +0800

    引入示例（业务代码），后面要把开发的权限控制用到业务上

--- permission_learn/settings.py
+++ permission_learn/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rbac.apps.RbacConfig',
+    'web.apps.WebConfig',
+
 ]
 
 MIDDLEWARE = [
