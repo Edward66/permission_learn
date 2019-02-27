bf6d85944c333dc3ddd4e6b9206cc55e5f418e3c
Edward <edwardlshfzw@gmail.com>
2019-02-27 10:48:11 +0800

    fix bug

--- rbac/service/init_permission.py
+++ rbac/service/init_permission.py
@@ -10,5 +10,5 @@ def init_permission(current_user, request):
     """
     permission_queryset = current_user.roles.filter(permissions__isnull=False).values('permissions__id',
                                                                                       'permissions__url').distinct()
-    permission_list = [item['permissions_url'] for item in permission_queryset]
+    permission_list = [item['permissions__url'] for item in permission_queryset]
     request.session[settings.PERMISSION_SESSION_KEY] = permission_list
